4e64f5696d44154825cd9bc798286e5bcde28b5c
ryohashioka <haru.love2and2rock@gmail.com>
2018-07-20 19:52:28 +0900

    プロジェクト作成／ログインページ？作成

---
+++ html/config.rb
@@ -0,0 +1,12 @@
+require 'compass/import-once/activate'
+
+# Set this to the root of your project when deployed:
+Encoding.default_external = "utf-8"
+http_path = "/" 
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+output_style = "expanded"
+line_comments = false
+relative_assets = true
